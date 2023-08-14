f7a5c151550f10552e57b642f9faa1fc04437980
deepanshudashora <deepanshudashora@gmail.com>
2023-08-14 20:20:54 +0530

    Model changes

--- YOLOv3/main.py
+++ YOLOv3/main.py
@@ -1,12 +1,17 @@
-from pytorch_lightning import LightningModule, Trainer, seed_everything
+from lightning.pytorch.callbacks import ProgressBar
+from lightning.pytorch import LightningModule, Trainer, seed_everything
 import torch 
 from torchmetrics import Accuracy
 import torch.optim as optim
-from pytorch_lightning import loggers as pl_loggers
-from pytorch_lightning.callbacks import LearningRateMonitor
-from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.loggers import CSVLogger
-from pytorch_lightning import Trainer
+from lightning.pytorch import loggers as pl_loggers
+from lightning.pytorch.callbacks import LearningRateMonitor
+from lightning.pytorch.callbacks.progress import TQDMProgressBar
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch import Trainer
+import pandas as pd 
+from IPython.core.display import display
+
+
 import pandas as pd 
 from custom_models.YOLOv3.loss import YoloLoss
 from IPython.core.display import display
@@ -66,70 +71,77 @@ def check_accuracy(x,y,model,threshold,device):
         tot_noobj += torch.sum(noobj)
         
 class YOLOTraining(LightningModule):
-    def __init__(self,loss_fn,config,model,max_lr,train_loader,check_accuracy,pct_start):
+    def __init__(self,loss_fn,config,model,max_lr,train_loader,pct_start):
         super().__init__()
-        self.train_accuracy = Accuracy(task="multiclass", num_classes=10)
-        self.val_accuracy = Accuracy(task="multiclass", num_classes=10)
+        #self.train_accuracy = Accuracy(task="multiclass", num_classes=20)
+        #self.val_accuracy = Accuracy(task="multiclass", num_classes=20)
         self.config = config
         self.loss_fn = loss_fn
-        self.scaled_anchors = (torch.tensor(self.config.ANCHORS)
-                                * torch.tensor(self.config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
-                            ).to(self.config.DEVICE)
+#         self.scaled_anchors = (torch.tensor(self.config.ANCHORS)
+#                                 * torch.tensor(self.config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+#                             )
+        self.ANCHORS = self.config.ANCHORS
+        self.S = self.config.S
         self.model = model
         self.max_lr = max_lr
         self.pct_start = pct_start
         self.train_loader = train_loader
-        self.check_accuracy = check_accuracy
         
     def forward(self, x):
         return self.model(x)
         
     def training_step(self, batch, batch_id):
         x,y=batch
-        x = x.to(self.config.DEVICE)
+        x = x
         y0, y1, y2 = (
-            y[0].to(self.config.DEVICE),
-            y[1].to(self.config.DEVICE),
-            y[2].to(self.config.DEVICE),
+            y[0],
+            y[1],
+            y[2],
         )
-
+        scaled_anchors = (
+            torch.tensor(self.ANCHORS)
+            * torch.tensor(self.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+        ).to(y0.device)
         with torch.cuda.amp.autocast():
             out = self(x)
             loss = (
-                self.loss_fn(out[0], y0, self.scaled_anchors[0])
-                + self.loss_fn(out[1], y1, self.scaled_anchors[1])
-                + self.loss_fn(out[2], y2, self.scaled_anchors[2])
+                self.loss_fn(out[0], y0, scaled_anchors[0])
+                + self.loss_fn(out[1], y1, scaled_anchors[1])
+                + self.loss_fn(out[2], y2, scaled_anchors[2])
             )
 
-            self.log(f"train_loss", loss, prog_bar=True)
+            self.log(f"train_loss", loss, prog_bar=True,sync_dist=True)
 
         return loss
     
     def evaluate(self, batch,stage=None):
         x,y=batch
-        x = x.to(self.config.DEVICE)
+        x = x
         y0, y1, y2 = (
-            y[0].to(self.config.DEVICE),
-            y[1].to(self.config.DEVICE),
-            y[2].to(self.config.DEVICE),
+            y[0],
+            y[1],
+            y[2],
         )
-
+        scaled_anchors = (
+            torch.tensor(self.ANCHORS)
+            * torch.tensor(self.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2)
+        ).to(y0.device)
         with torch.cuda.amp.autocast():
             out = self(x)
             loss = (
-                self.loss_fn(out[0], y0, self.scaled_anchors[0])
-                + self.loss_fn(out[1], y1, self.scaled_anchors[1])
-                + self.loss_fn(out[2], y2, self.scaled_anchors[2])
+                self.loss_fn(out[0], y0, scaled_anchors[0])
+                + self.loss_fn(out[1], y1, scaled_anchors[1])
+                + self.loss_fn(out[2], y2, scaled_anchors[2])
             )
-            val_accuracy = check_accuracy(x,y,self.model,self.config.CONF_THRESHOLD,self.config.DEVICE)
-            if val_accuracy!=None:
-                clas_acc,no_obj_acc,obj_acc = val_accuracy[0],val_accuracy[1],val_accuracy[2]
-                self.log(f"{stage}_loss", loss, prog_bar=True)
-                self.log(f"{stage}_clasacc", clas_acc, prog_bar=True)
-                self.log(f"{stage}_no_obj_acc", no_obj_acc, prog_bar=True)
-                self.log(f"{stage}_obj_acc", obj_acc, prog_bar=True)
-            else:
-                self.log(f"{stage}_loss", loss, prog_bar=True)
+            #             val_accuracy = check_accuracy(x,y,self.model,self.config.CONF_THRESHOLD,"cuda")
+            #             if val_accuracy!=None:
+            #                 clas_acc,no_obj_acc,obj_acc = val_accuracy[0],val_accuracy[1],val_accuracy[2]
+            #                 self.log(f"{stage}_loss", loss, prog_bar=True,sync_dist=True)
+            #                 self.log(f"{stage}_clasacc", clas_acc, prog_bar=True,sync_dist=True)
+            #                 self.log(f"{stage}_no_obj_acc", no_obj_acc, prog_bar=True,sync_dist=True)
+            #                 self.log(f"{stage}_obj_acc", obj_acc, prog_bar=True,sync_dist=True)
+            #             else:
+            self.log(f"{stage}_loss", loss, prog_bar=True,sync_dist=True)
 
     def validation_step(self, batch, batch_idx):
         self.evaluate(batch, "val")
@@ -140,7 +152,7 @@ class YOLOTraining(LightningModule):
         val_loss = self.trainer.callback_metrics["val_loss"]
 
         # Print train and validation losses
-        print(f"Epoch {self.current_epoch}: Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}")
+        self.log(f"Epoch {self.current_epoch}: Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}",prog_bar=True,sync_dist=True)
 
     def test_step(self, batch, batch_idx):
         self.evaluate(batch, "test")
@@ -170,19 +182,20 @@ def make_trainer(max_epochs,train_loader, test_loader, train_eval_loader,max_lr,
                  csv_logs = "csv_training_logs/"
             ):
     tb_logger = pl_loggers.TensorBoardLogger(tensorboard_logs)
-    csv_logger = CSVLogger(save_dir=csv_logs)
-    #loss_fn,config,model,max_lr,train_loader,check_accuracy,pct_start
+    csv_logger = CSVLogger(save_dir=csv_logs)    
     loss_fn = YoloLoss()
-    model = YOLOv3(num_classes=config.NUM_CLASSES).to(config.DEVICE)
-    model = YOLOTraining(loss_fn,config,model,max_lr,train_loader,check_accuracy,5/max_epochs)
+    model = YOLOv3(num_classes=config.NUM_CLASSES)
+    model = YOLOTraining(loss_fn,config,model,max_lr,train_loader,5/max_epochs)
     data_module = YOLODataModule(train_loader, test_loader, train_eval_loader)
+    from lightning.pytorch.strategies import DDPStrategy 
     trainer = Trainer(
         max_epochs=max_epochs,
-        accelerator=accelerator,
-        devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
+        accelerator="gpu", 
+        devices=2, 
+        strategy="ddp_notebook_find_unused_parameters_true",
         logger=[tb_logger, csv_logger],
-        precision=precision,
-        callbacks=[LearningRateMonitor(logging_interval="step"), TQDMProgressBar(refresh_rate=refresh_rate)],
+        precision=16,
+        callbacks=[LearningRateMonitor(logging_interval="step"), TQDMProgressBar(refresh_rate=10)],
         check_val_every_n_epoch=check_val_every_n_epoch
     )
     trainer.fit(model,data_module)
