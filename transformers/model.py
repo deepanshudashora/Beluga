d0ba92fc81d7288d634da8ac70bf53e53ff177c3
deepanshudashora <deepanshudashora@gmail.com>
2023-09-10 17:29:41 +0530

    Added transformers datamodules

--- transformers/model.py
+++ transformers/model.py
@@ -13,7 +13,8 @@ import torch
 import math
 import re
 
-
+from vit.model import PatchEmbedding
+from vit.model import TransformerEncoderBlock
 # =============================================================================
 # Transformer
 # =============================================================================
@@ -213,10 +214,29 @@ class TransformerBlock(nn.Module):
         return x
     
 class Transformer(nn.Module):
-    def __init__(self, n_code=None, n_heads=4,block_size=8,
-                 num_layers=4, embed_size=32, inner_ff_size=None
-                , n_embeddings=None, seq_len=None,vocab_size=100, 
-                dropout=.1,algorithm="BERT"):
+    def __init__(self, n_code=None,
+                 n_heads=4,
+                 block_size=8,
+                 num_layers=4, 
+                 embed_size=32, 
+                 inner_ff_size=None
+                , n_embeddings=None,
+                seq_len=None,
+                vocab_size=100, 
+                img_size:int=224, # Training resolution from Table 3 in ViT paper
+                in_channels:int=3, # Number of channels in input image
+                embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                attn_dropout:float=0, # Dropout for attention projection
+                mlp_dropout:float=0.1, # Dropout for dense/MLP layers 
+                embedding_dropout:float=0.1, # Dropout for patch and position embeddings,
+                num_classes:int=1000, # Default for ImageNet but can customize this
+                dropout=0.1,
+                algorithm="BERT"):
+    
+        # N_heads = 12 for VIT
+        # block_size == patch_size = 16 for vit
+        # num_layers = 12 for VIT:
         super().__init__()
         
         self.algorithm = algorithm
@@ -262,6 +282,43 @@ class Transformer(nn.Module):
             # we add the layer norm before the Linear layer
             self.ln_f = nn.LayerNorm(self.num_embed)
             self.lm_head = nn.Linear(self.num_embed, self.vocab_size)
+            
+        elif self.algorithm == "VIT":
+            # 3. Make the image size is divisble by the patch size 
+            assert img_size % block_size == 0, f"Image size must be divisible by patch size, image size: {img_size}, patch size: {block_size}."
+            
+            # 4. Calculate number of patches (height * width/patch^2)
+            self.num_patches = (img_size * img_size) // block_size**2
+                    
+            # 5. Create learnable class embedding (needs to go at front of sequence of patch embeddings)
+            self.class_embedding = nn.Parameter(data=torch.randn(1, 1, embedding_dim),
+                                                requires_grad=True)
+            
+            # 6. Create learnable position embedding
+            self.position_embedding = nn.Parameter(data=torch.randn(1, self.num_patches+1, embedding_dim),
+                                                requires_grad=True)
+                    
+            # 7. Create embedding dropout value
+            self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+            
+            # 8. Create patch embedding layer
+            self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                                patch_size=block_size,
+                                                embedding_dim=embedding_dim)
+            
+            # 9. Create Transformer Encoder blocks (we can stack Transformer Encoder blocks using nn.Sequential()) 
+            # Note: The "*" means "all"
+            self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                                num_heads=n_heads,
+                                                                                mlp_size=mlp_size,
+                                                                                mlp_dropout=mlp_dropout) for _ in range(num_layers)])
+        
+            # 10. Create classifier head
+            self.classifier = nn.Sequential(
+                nn.LayerNorm(normalized_shape=embedding_dim),
+                nn.Linear(in_features=embedding_dim, 
+                        out_features=num_classes)
+            )
     
     def forward(self, x,targets=None):
         if self.algorithm == "BERT":
@@ -272,6 +329,7 @@ class Transformer(nn.Module):
             x = self.norm(x)
             x = self.linear(x)
             return x
+        
         elif self.algorithm == "GPT":
             B, T = x.shape
             # idx and targets are (B,T) tensor of integers
@@ -298,6 +356,33 @@ class Transformer(nn.Module):
             else:
                 loss = None
             return logits, loss
+        
+        elif self.algorithm == "GPT":
+            # 12. Get batch size
+            batch_size = x.shape[0]
+            
+            # 13. Create class token embedding and expand it to match the batch size (equation 1)
+            class_token = self.class_embedding.expand(batch_size, -1, -1) # "-1" means to infer the dimension (try this line on its own)
+
+            # 14. Create patch embedding (equation 1)
+            x = self.patch_embedding(x)
+
+            # 15. Concat class embedding and patch embedding (equation 1)
+            x = torch.cat((class_token, x), dim=1)
+
+            # 16. Add position embedding to patch embedding (equation 1) 
+            x = self.position_embedding + x
+
+            # 17. Run embedding dropout (Appendix B.1)
+            x = self.embedding_dropout(x)
+
+            # 18. Pass patch, position and class embedding through transformer encoder layers (equations 2 & 3)
+            x = self.transformer_encoder(x)
+
+            # 19. Put 0 index logit through classifier (equation 4)
+            x = self.classifier(x[:, 0]) # run on each sample in a batch at 0 index
+
+            return x   
 
     def generate(self, idx: torch.Tensor, max_new_tokens: int, block_size: int):
         # idx is (B, T) array of indices in the current context
